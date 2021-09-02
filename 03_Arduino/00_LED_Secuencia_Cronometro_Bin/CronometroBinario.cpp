a8ec6f9ddea020341d2964ccb4dc01b2c68b3e57
Facundo Falcone <amilcar.f.falcone@accenture.com>
2021-09-02 18:20:53 -0300

    Nuevos Ejercicios Arduino

---
+++ 03_Arduino/00_LED_Secuencia_Cronometro_Bin/CronometroBinario.cpp
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [SPD]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+*/
+
+/*
+	Ejercicio de práctica: Cronómetro Binario
+    
+    Al iniciar la simulacion, todos los led deben comenzar apagados.
+    Cada 1 segundo que transcurra, se deben encender los led
+    de acuerdo al numero que representan
+
+	Ejemplo:
+    
+    El numero 15 (001111) se representaria encendiendo los leds
+    AZUL, NARANJA, AMARILLO, VERDE
+    Al pasar al numero 16 (010000), se deben apagar todos los led
+    anteriores y encenderse el ROJO
+    
+    (Notese, en el ejemplo los 0 representan los led apagados, los
+    1 los led encendidos)
+    
+    Tip: Modularizar la funcion que controla el encendido de los 
+    LED y utilizar recursividad
+*/
+void setup(){
+	
+}
+
+
+void loop(){
+  
+}
---
+++ 03_Arduino/00_LED_Secuencia_Semaforo/Semaforo.cpp
@@ -0,0 +1,51 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [SPD]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+*/
+
+/*
+	Ejercicio: Semáforo 1
+    
+    Objetivo: Utilizando la lógica aprendida en Arduino, replicar el
+    comportamiento de un semáforo.
+    
+    Al inicio del programa, se debe encender el LED ROJO del
+    semáforo de arriba (pin 7). Tras 5
+    segundos, debe alternar al LED AMARILLO por 2 segundos y final-
+    mente al verde, por otros 5 segundos. Luego, debe volver a
+    transicionar hasta el led rojo, pasando por el amarillo.
+    
+    El semáforo de abajo debe ir opuesto al de arriba (Cuando se 
+    enciende el LED ROJO del semaforo de arriba, el de abajo
+    debe tener encendido el verde y viceversa)
+
+*/
+
+void setup(){
+
+}
+
+
+void loop(){
+  
+}
